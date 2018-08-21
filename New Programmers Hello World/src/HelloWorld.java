87c95a135dedbb282dc6c29efe21d44f087aa0c9
zachlloyd01 <zachlloyd01@gmail.com>
2018-08-21 16:57:21 -0400

    Their first program!!!
    A hello world program printing to the console

---
+++ New Programmers Hello World/src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+	//text for our string
+	public static String Christen = "Hello world";
+	public static void main(String[] args) {
+		while (1 == 1) {
+			System.out.println(Christen);
+		}
+	}
+}
